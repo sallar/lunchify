54d8676cfdd4e09eface6bed5733c3535ac4cf23
Sallar Kaboli <sallar.kaboli@gmail.com>
2015-05-21 19:42:14 +0300

    Updated Scraper, New Data

--- scraper/app.js
+++ scraper/app.js
@@ -1,67 +1,81 @@
 /**
  * Created by sallar on 5/21/15.
  */
-var path = [],
-    routes = [
-        "http://lounaat.info/lounas/ravintola-keilalahden-ranta/espoo",
-        "http://lounaat.info/lounas/swingin-soupsalad-bar-k4/espoo",
-        "http://lounaat.info/lounas/think/espoo",
-        "http://lounaat.info/lounas/be-keilaranta/espoo",
-        "http://lounaat.info/lounas/ravintola-1951/espoo",
-        "http://lounaat.info/lounas/life-science-center/espoo"
-    ];
-
 var request = require("request");
 var cheerio = require("cheerio");
 
-routes.forEach(function(route) {
-    var options = {
-        url: route,
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'
-        }
-    };
+String.prototype.lpad = function(padString, length) {
+    var str = this;
+    while (str.length < length)
+        str = padString + str;
+    return str;
+};
 
-    request(options, function(error, response, body) {
+request({url: 'https://lunchify.firebaseio.com/areas/keilaniemi/venues.json'}, function(err, resp, body) {
+    var json = JSON.parse(body),
+        dates = {};
 
-        var $ = cheerio.load(body);
+
+    json.forEach(function(venue) {
+
+        var options = {
+            url: 'http://lounaat.info/lounas/' + venue.simple_name + '/espoo',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'
+            }
+        };
+
+        request(options, function(error, response, body) {
+
+            var $ = cheerio.load(body);
 
 
-        // Items
-        var items = $('.item', $('.item-container')[0]),
-            data  = [];
+            // Items
+            var items = $('.item', $('.item-container')[0]),
+                data  = {};
 
-        items.each(function() {
-            var date = $('.item-header h3', this).text();
-            var menuEl = $('.item-body li p', this);
-            var menu = [];
-            var meals = [];
+            items.each(function() {
+                var date = $('.item-header h3', this).text();
+                var menuEl = $('.item-body li p', this);
+                var menu = [];
+                var meals = [];
 
-            menuEl.each(function() {
-                menu.push($(this).html());
+                menuEl.each(function() {
+                    menu.push($(this).html());
+                });
+
+                for(var i = 0; i < menu.length; i += 2) {
+                    meals.push({
+                        name_fi: menu[i],
+                        name: menu[i+1]
+                    });
+                }
+
+                if( date.match(/(\d+)/) ) {
+                    date = date.match(/(\d+)/g);
+                }
+                //console.log(date);
+                if( typeof date == 'object' ) {
+                    var dateStr = '2015' + '-' + date[1].lpad("0", 2) + '-' + date[0].lpad("0", 2);
+                    data[dateStr] = meals;
+                }
             });
 
-            for(var i = 0; i < menu.length; i += 2) {
-                meals.push({
-                    name: menu[i],
-                    name_fi: menu[i+1]
-                });
-            }
+            //dates[venue.id] = data;
 
-            if( date.match(/(\d+)/) ) {
-                date = date.match(/(\d+)/g);
-            }
-            //console.log(date);
-            if( typeof date == 'object' ) {
-                data.push({
-                    month: date[1] || null,
-                    day: date[0] || null,
-                    meals: meals
-                });
-            }
+            var api = 'https://lunchify.firebaseio.com/areas/keilaniemi/meals/'+venue.id+'.json';
+
+            request({
+                method: 'PUT',
+                uri: api,
+                body: data,
+                json: true
+            }, function(err, resp, body) {
+                console.log('done');
+            });
+
         });
 
-        console.log(route, data);
-
     });
+
 });
